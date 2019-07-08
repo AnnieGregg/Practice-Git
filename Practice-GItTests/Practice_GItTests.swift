1002c33e67c367e980ebc8664939c425146c62d6
Apple <apple@Apples-MacBook-Pro-3.local>
2019-07-08 15:22:33 -0500

    First GitHub Upload

---
+++ Practice-GItTests/Practice_GItTests.swift
@@ -0,0 +1,34 @@
+//
+//  Practice_GItTests.swift
+//  Practice-GItTests
+//
+//  Created by Apple on 7/8/19.
+//  Copyright © 2019 Kode with Klossy. All rights reserved.
+//
+
+import XCTest
+@testable import Practice_GIt
+
+class Practice_GItTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
